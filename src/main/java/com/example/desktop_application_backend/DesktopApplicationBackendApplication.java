c928b585e938c1a685070bdd0b12ca7133b7a678
AJPraneeth <praneethdhananjaya99@gmail.com>
2024-06-01 14:31:36 +0530

    second commit

--- src/main/java/com/example/desktop_application_backend/DesktopApplicationBackendApplication.java
+++ src/main/java/com/example/desktop_application_backend/DesktopApplicationBackendApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DesktopApplicationBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(DesktopApplicationBackendApplication.class, args);
 	}
+
+
 
 }
