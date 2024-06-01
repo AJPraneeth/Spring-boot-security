b4a9266da10ff1619b17ccf30503a06eeff77b2e
AJPraneeth <praneethdhananjaya99@gmail.com>
2024-06-01 14:20:55 +0530

    first commit

---
+++ src/main/java/com/example/desktop_application_backend/DesktopApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.desktop_application_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesktopApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesktopApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desktop_application_backend/DesktopApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desktop_application_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesktopApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
